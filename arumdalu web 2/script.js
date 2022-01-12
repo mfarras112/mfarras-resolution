d0bdbc357f111c374bc459bd65cd57bbda9c9358
mfarras <93836343+mfarras112@users.noreply.github.com>
2022-01-12 23:54:05 +0700

    Delete arumdalu web 2 directory

--- arumdalu web 2/script.js
+++
@@ -1,8 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle input');
-const nav = document.querySelector('nav ul');
-
-// Ketika toggle di klik, jalankan function berikut.
-menuToggle.addEventListener('click', function () {
-  // toogle itu kalo gak ada classnya tambahin, kalo ada classnya hilangin, disini kelas yg ingin ditambahin adalah slide
-  nav.classList.toggle('slide');
-});
